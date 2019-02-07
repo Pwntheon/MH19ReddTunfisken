1bd493de7744dc0c41f881fba25be958ebe2d984
Christian Linchausen <christian.linchausen@knowit.no>
2019-02-07 16:26:49 +0100

    Login and redirect.

---
+++ frontend/pages/login/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Router from 'next/router';
+import { connect } from 'react-redux';
+import { login } from '../../store';
+import css from '../../styles.less';
+
+class Login extends Component {
+  render() {
+    const authCallback = response => {
+      console.log(response);
+      const { dispatch } = this.props;
+      dispatch(login(response));
+      Router.push('/index');
+    };
+
+    return (
+      <Jumbotron>
+        <h1>Logg inn!</h1>
+        <p>Logg inn via Facebook for å bli med da vel :)</p>
+        <FacebookLogin
+          appId="574897169694887"
+          autoLoad={true}
+          fields="name,email,picture"
+          callback={response => authCallback(response)}
+        />
+      </Jumbotron>
+    );
+  }
+}
+
+export default connect()(Login);
