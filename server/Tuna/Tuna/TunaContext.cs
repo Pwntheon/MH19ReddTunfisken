4577b4288919cd9aa0d02ca3efd9dc3764e3230a
Kristoffer Berge <Kristoffer.Berge@knowit.no>
2019-02-07 10:40:10 +0100

    Adding API

---
+++ server/Tuna/Tuna/TunaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tuna
+{
+    public class TunaContext : DbContext
+    {
+        public TunaContext(DbContextOptions<TunaContext> options) : base(options)
+        {
+        }
+        //public DbSet<Post> Posts { get; set; }
+    }
+}
