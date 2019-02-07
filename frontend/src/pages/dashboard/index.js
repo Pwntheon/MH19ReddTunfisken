de237daaacf0508fb64e0434158dc6a0c610c133
Aksel Ladegaard <aksel.ladegaard@gmail.com>
2019-02-07 20:59:30 +0100

    Add trashometer component with simple debugging slider

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import Trashometer from './trashometer/trashometer';
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temp: 80
+    };
+  }
   render() {
     const { authentication } = this.props;
 
     if (!authentication.accessToken) {
       return <Redirect to="/login" />;
     }
-    console.log(authentication);
-    return 'Dashboard';
+    return (
+      <>
+        <Trashometer temp={this.state.temp} />
+        <input type="range" min="50" max="150" value={this.state.temp} onChange={e => this.setState({temp:e.target.value})}/>
+      </>
+    );
   }
 }
 
