225e7508fb20c30ed910588c6fa3d299cba3fade
Christian Linchausen <christian.linchausen@knowit.no>
2019-02-08 20:05:38 +0100

    Style predictions page

--- frontend/src/components/Predictions.js
+++ frontend/src/components/Predictions.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-
+import './Predictions.css';
 const tagDescriptions = {
-  papp: 'Papp skal i papirsøppelet',
-  plast: 'Plast skal i plastsøppelet - Blå pose',
+  papp:
+    'Papp skal i papirsøppelet. Husk å brette pappen godt sammen slik det blir bedre plass i containeren.',
+  plast:
+    'Plast skal i blå pose. Husk å skylle emballasjen med kaldt vann før du kaster den.',
   metall: 'Metall skal i glass/metallsøppelet',
   restavfall: 'Restavfall skal i restavfallet',
   pant: 'Pant alt, alltid',
   tekstiler: 'Tekstiler skal leveres til klesbutikker/miljøstasjon',
   matavfall: 'Matavfall skal i grønne poser',
-  glass: 'Glass skal i glass/metallsøppelet'
+  glass: 'Glass skal i glass/metallsøppelet',
+  venn: 'Venner skal man være snill med'
 };
 
 const Prediction = props => {
@@ -19,7 +22,7 @@ const Prediction = props => {
   const desc = tagDescriptions[props.prediction.tagName];
   return (
     <li key={props.prediction.tagId}>
-      Dette er en {props.prediction.tagName}. {desc}. Vi er{' '}
+      Dette er {props.prediction.tagName}. {desc}. Vi er{' '}
       {Math.round(props.prediction.probability * 100)}% på denne antagelsen.
     </li>
   );
@@ -32,7 +35,7 @@ class Predictions extends Component {
     const listItems = predictions.map(prediction => (
       <Prediction key={prediction.tagId} prediction={prediction} />
     ));
-    return <ul>{listItems}</ul>;
+    return <ul className="predictions">{listItems}</ul>;
   }
 }
 
