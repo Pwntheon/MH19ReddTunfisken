1dafddeaec3d90ca8676f0efe4599babeaa78ef2
Kristoffer Berge <Kristoffer.Berge@knowit.no>
2019-02-08 11:09:01 +0100

    Calculating bus distance based on food waste collection

---
+++ server/Tuna/Tuna/Models/BusDistanceStatistics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tuna.Models
+{
+    public class BusDistanceStatistics
+    {
+        public string ReadableDistance { get; set; }
+        public int DistanceInMeters { get; set; }
+        public string GeographicDistanceDescription { get; set; }
+    }
+}
